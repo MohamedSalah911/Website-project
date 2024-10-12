b6461feb1a955443acbcc161b92c977d700a9b64
Kareem Ali <kareem.ali.0750@gmail.com>
2024-10-12 22:12:33 +0300

    modify the product modal content

--- src/components/modals/prodcut-modal.jsx
+++ src/components/modals/prodcut-modal.jsx
@@ -1,43 +1,78 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
-  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog"
-import { useModal } from "@/../hooks/use-modal-store"
-import { formatter } from "@/lib/utils"
-
+} from "@/components/ui/dialog";
+import { useModal } from "@/../hooks/use-modal-store";
+import { formatter } from "@/lib/utils";
 
 export const ProductModal = () => {
-  const {isOpen, type, onClose, data} = useModal()
-  const {product} = data
-  const isModalOpened = isOpen && type === "showProduct" 
-  //TODO: use thie vairables and style the modal of the product to match the figma design or you can design as you like */
- const  title = product?.name
-  const colorHex = product?.color?.value
- const  color = product?.color.name
-const   category = product?.category.name
- const  size = product?.size.value
-  const image = product?.images[0]
-  const price = formatter.format(Number(product?.price))
+  const { isOpen, type, onClose, data } = useModal();
+  const { product } = data;
+  const isModalOpened = isOpen && type === "showProduct";
+
+  // Extract product details
+  const title = product?.name;
+  const colorHex = product?.color?.value;
+  const colors = product?.availableColors; // Assuming there is a list of available colors
+  const category = product?.category.name;
+  const price = formatter.format(Number(product?.price));
+  const image = product?.images[0]; // Main product image
+
   return (
     <Dialog open={isModalOpened} onOpenChange={onClose}>
-     <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle></DialogTitle>
-          <DialogDescription>
-            Make changes to your profile here. Click save when you're done.
-          </DialogDescription>
-        </DialogHeader>
-        
-        <DialogFooter>
-          <Button type="submit">Save changes</Button>
-        </DialogFooter>
+      <DialogContent className="sm:max-w-[800px] flex flex-row space-x-6 p-6">
+        {/* Left side: Images */}
+        <div className="flex flex-col space-y-4">
+          <img src={image} alt={title} className="w-64 h-64 object-cover" />
+          <div className="flex space-x-2">
+            {/* Thumbnails - assuming you have multiple images */}
+            {product?.images.map((img, idx) => (
+              <img
+                key={idx}
+                src={img}
+                alt={`Thumbnail ${idx}`}
+                className="w-16 h-16 object-cover border"
+              />
+            ))}
+          </div>
+        </div>
+
+        {/* Right side: Product details */}
+        <div className="flex flex-col space-y-4">
+          <DialogHeader>
+            <DialogTitle className="text-2xl font-bold">{title}</DialogTitle>
+          </DialogHeader>
+          
+          <div className="text-lg">
+            <span className="font-semibold">Category: </span>
+            {category}
+          </div>
+          
+          <div className="flex items-center space-x-2">
+            <span className="font-semibold">Colors: </span>
+            <div className="flex space-x-2">
+              {colors?.map((color, idx) => (
+                <div
+                  key={idx}
+                  className="w-6 h-6 rounded-full"
+                  style={{ backgroundColor: color.hexValue }} // Color hex value for background
+                ></div>
+              ))}
+            </div>
+          </div>
+          
+          <div className="text-lg">
+            <span className="font-semibold">Price: </span>
+            {price}
+          </div>
+
+          <Button className="bg-black text-white p-3 rounded-md">Add to Cart</Button>
+        </div>
       </DialogContent>
     </Dialog>
-  )
-}
+  );
+};
